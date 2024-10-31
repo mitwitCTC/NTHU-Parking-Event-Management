631ebec0f716232e9a63fbce6b3695c3adb6afd2
Eileen-Qian <yiqianc.14@gmail.com>
2024-10-31 17:49:29 +0800

    feat: Implement bootstrap and vue-i18n
    remove dark mode style setting (vite)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
+import zh from '@/locales/zh.json'
+const i18n = createI18n({
+  locale: 'zh', // 預設語言
+  fallbackLocale: 'zh', // 當找不到對應語言時使用的語言
+  messages: {
+    zh,
+  },
+})
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +19,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
