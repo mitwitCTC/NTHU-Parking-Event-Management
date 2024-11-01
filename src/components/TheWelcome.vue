a5953eb0421eb8118d9db79fa7d6d00092a9564f
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-01 14:55:38 +0800

    feat: Set layout & add en locale
    change theme color

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,13 +1,62 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted, onUnmounted, computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { locale } = useI18n()
+
+const languageDisplay = computed(() => {
+  return locale.value === 'zh' ? '中文/EN' : '中文/EN'
+})
+const toggleLanguage = () => {
+  locale.value = locale.value === 'zh' ? 'en' : 'zh'
+}
+
+const contentHeight = ref('auto') // 儲存 .content 高度的狀態
+// 動態計算 .content 高度
+const setContentHeight = () => {
+  const bgSecondary = document.querySelector('.bg-secondary')
+  const bgSecondaryHeight = bgSecondary ? bgSecondary.offsetHeight : 0
+  contentHeight.value = `calc(100vh - ${bgSecondaryHeight}px)`
+}
+
+// 在組件掛載時設定 .content 高度
+onMounted(() => {
+  setContentHeight()
+  // 監聽視窗大小變化，視窗改變時重新計算高度
+  window.addEventListener('resize', setContentHeight)
+})
+
+// 清除監聽事件
+onUnmounted(() => {
+  window.removeEventListener('resize', setContentHeight)
+})
+</script>
 
 <template>
-  <h1>首頁</h1>
-  <router-link to="queryLinks">駐警隊申請查詢</router-link> |
-  <router-link to="/applyFacultyStudentParking">
-    教職員工生停車證申請
-  </router-link>
-  | <router-link to="/applyStaffParking">工作人員停車證申請</router-link> |
-  <router-link to="/applyEvent">各單位自辦活動申請</router-link> |
-  <router-link to="/applyEventCoupon">自辦活動&抵用券申請</router-link> |
-  <router-link to="/preorderCoupon">預購抵用券</router-link>
+  <div class="bg-secondary text-center">
+    <div class="header text-white">
+      <h2>{{ $t('pages.index.title') }}</h2>
+      <h5>{{ $t('pages.index.subtitle') }}</h5>
+    </div>
+    <div class="bg-light mt-0 py-3 header-btn-section">
+      <div class="d-flex justify-content-end pe-3">
+        <button class="btn btn-primary" @click="toggleLanguage">
+          {{ languageDisplay }}
+        </button>
+      </div>
+    </div>
+  </div>
+  <div class="bg-light content" :style="{ height: contentHeight }">
+    {{ $t('pages.index.queryLinks') }}
+  </div>
 </template>
+
+<style scoped>
+.header {
+  padding: 20px;
+}
+
+.header-btn-section {
+  border-radius: 50px 0 0 0;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createI18n } from 'vue-i18n'
 import zh from '@/locales/zh.json'
+import en from '@/locales/en.json'
 const i18n = createI18n({
   locale: 'zh', // 預設語言
   fallbackLocale: 'zh', // 當找不到對應語言時使用的語言
   messages: {
     zh,
+    en,
   },
 })
 
