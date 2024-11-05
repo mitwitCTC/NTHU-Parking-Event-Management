b492bdc793700e9cbf3989ad8837f8d79df9ec52
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-05 15:19:00 +0800

    feat: Updae router path and name

--- src/views/queryLinks.vue
+++ src/views/queryLinks.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <p>搜尋首頁</p>
-    <router-link to="/queryFacultyStudentParking">
+    <router-link to="/query-faculty-student-parking">
       教職員工生停車證查詢
     </router-link>
-    | <router-link to="/queryStaffParking"> 工作人員停車證查詢 </router-link> |
-    <router-link to="/queryEvent"> 自辦活動活動查詢 </router-link> |
-    <router-link to="/queryCoupon"> 活動及預購抵用券查詢 </router-link>
+    |
+    <router-link to="/query-staff-parking"> 工作人員停車證查詢 </router-link> |
+    <router-link to="/query-event"> 自辦活動活動查詢 </router-link> |
+    <router-link to="/query-coupon"> 活動及預購抵用券查詢 </router-link>
   </div>
 </template>
