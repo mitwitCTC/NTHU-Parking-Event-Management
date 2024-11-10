16f1a673ad3caa1ff120983efa7db66efc9254cc
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-11 02:27:11 +0800

    feat: add VehicleType component with vehicle selection and status update in parent

---
+++ src/components/applyStaffParking/VehicleType.vue
@@ -0,0 +1,68 @@
+<script setup>
+// 定義接收的 props 和 emits
+import { defineProps, defineEmits } from 'vue'
+
+defineProps({
+  vehicleType: {
+    type: String,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['updateVehicleType'])
+
+function handleClick(type) {
+  emit('updateVehicleType', type)
+}
+</script>
+
+<template>
+  <section>
+    <div class="d-flex justify-content-start gap-5">
+      <div
+        class="d-flex flex-column align-items-center pointer"
+        :class="{ 'gray-overlay': vehicleType != 'WC' }"
+        @click="handleClick('WC')"
+      >
+        <div class="img-container">
+          <img src="/images/vehicle/汽車.svg" alt="汽車" />
+        </div>
+      </div>
+      <div
+        class="d-flex flex-column align-items-center pointer"
+        :class="{ 'gray-overlay': vehicleType != 'WM' }"
+        @click="handleClick('WM')"
+      >
+        <div class="img-container">
+          <img src="/images/vehicle/機車.svg" alt="機車" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+
+.gray-overlay {
+  position: relative;
+  opacity: 0.5;
+}
+
+.img-container {
+  background: #ffffff;
+  box-shadow:
+    rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  border-radius: 15px;
+  padding: 10px;
+  width: 80px;
+  height: 80px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/applyStaffParking/Step2VehicleRegistration.vue
+++ src/views/applyStaffParking/Step2VehicleRegistration.vue
@@ -1,5 +1,27 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useStaffStore } from '@/stores/staffStore'
+import VehicleType from '@/components/applyStaffParking/VehicleType.vue'
+
+const staffStore = useStaffStore()
+const vehicle_type = ref('WC') // 預設為汽車
+
+onMounted(() => {
+  staffStore.getApplicantData()
+})
+
+// 更新車輛類型( 車證型態：汽車/機車 )
+function updateVehicleType(type) {
+  vehicle_type.value = type
+}
+</script>
+
 <template>
-  <div>步驟二</div>
-  <h2>{{ $t('pages.applyStaffParking.title') }}</h2>
-  <h5>{{ $t('pages.applyStaffParking.subtitle') }}</h5>
+  <p class="fw-bold">
+    {{ $t('pages.applyStaffParking.vehicle_registration.vehicle_type') }}
+  </p>
+  <VehicleType
+    :vehicleType="vehicle_type"
+    @updateVehicleType="updateVehicleType"
+  />
 </template>
