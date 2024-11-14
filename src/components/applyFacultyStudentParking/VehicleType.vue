3daad03972901754fbdf1c4550c3fa15789398e3
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-14 19:57:48 +0800

    feat: add VehicleType component with vehicle selection and status update in parent (applyFacultyStudentParking)

---
+++ src/components/applyFacultyStudentParking/VehicleType.vue
@@ -0,0 +1,82 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+defineProps({
+  car_type_title: {
+    type: String,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['updateCar_type_title'])
+
+function handleClick(item) {
+  emit('updateCar_type_title', item)
+}
+</script>
+
+<template>
+  <section>
+    <div class="d-flex justify-content-start gap-5">
+      <div
+        class="d-flex flex-column align-items-center pointer"
+        :class="{
+          'gray-overlay': car_type_title != '汽車',
+        }"
+        @click="handleClick('汽車')"
+      >
+        <div class="img-container">
+          <img src="/images/vehicle/汽車.svg" alt="汽車" />
+        </div>
+      </div>
+      <div
+        class="d-flex flex-column align-items-center pointer"
+        :class="{
+          'gray-overlay': car_type_title != '機車',
+        }"
+        @click="handleClick('機車')"
+      >
+        <div class="img-container">
+          <img src="/images/vehicle/機車.svg" alt="機車" />
+        </div>
+      </div>
+      <div
+        class="d-flex flex-column align-items-center pointer"
+        :class="{
+          'gray-overlay': car_type_title != '腳踏車',
+        }"
+        @click="handleClick('腳踏車')"
+      >
+        <div class="img-container">
+          <img src="/images/vehicle/腳踏車.svg" alt="腳踏車" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+
+.gray-overlay {
+  position: relative;
+  opacity: 0.5;
+}
+
+.img-container {
+  background: #ffffff;
+  box-shadow:
+    rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+  border-radius: 15px;
+  padding: 10px;
+  width: 80px;
+  height: 80px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/applyFacultyStudentParking/Step2VehicleRegistration.vue
+++ src/views/applyFacultyStudentParking/Step2VehicleRegistration.vue
@@ -1,4 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import VehicleType from '@/components/applyFacultyStudentParking/VehicleType.vue'
+
+// 車輛類別
+const car_type_title = ref('汽車') // 預設為汽車
+
+// 更新車輛類別
+function updateCar_type_title(item) {
+  car_type_title.value = item
+}
+</script>
 <template>
-  <div>登記車輛</div>
+  <VehicleType
+    :car_type_title="car_type_title"
+    @updateCar_type_title="updateCar_type_title"
+  />
   <router-link to="/apply-faculty-student-parking/step3">上傳資料</router-link>
 </template>
