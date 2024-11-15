c9ef1759e4d2c09537e80298ae43522f8986f616
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-15 11:03:41 +0800

    feat: Add uploadDocuments for form field  in step3 (applyFacultyStudentParking)
    update locales data

--- src/views/applyFacultyStudentParking/Step3UploadDocuments.vue
+++ src/views/applyFacultyStudentParking/Step3UploadDocuments.vue
@@ -3,7 +3,12 @@ import { onMounted, ref } from 'vue'
 import { useFacultyStudentStore } from '@/stores/facultyStudentStore'
 const facultyStudentStore = useFacultyStudentStore()
 
-const applicationData = ref({})
+const applicationData = ref({
+  vehicle_registered_list: [],
+  selectedCommuteDistance: '',
+  document_list: Array(5).fill(null),
+  campusToReceiveCertificate: '1', // 預設在校本部領證
+})
 onMounted(() => {
   facultyStudentStore.getVehicleRegisteredList() // 取得 vehicle_registered_list
   applicationData.value.vehicle_registered_list =
@@ -31,12 +36,26 @@ const commuteDistances = [
 ]
 // 被選中的通勤距離（假設初始為5-10km）
 applicationData.value.selectedCommuteDistance = '5-10km'
+
+// 上傳證件
+function handleFileUpload(event, index) {
+  const file = event.target.files[0]
+  if (file) {
+    applicationData.value.document_list[index] = file
+  }
+}
+// 刪除上傳的證件
+function removeFile(index) {
+  applicationData.value.document_list[index] = null
+}
 </script>
 
 <template>
   <section>
     <p class="mt-3">
-      {{ $t('pages.applyStaffParking.uploadDocuments.plate_registered') }}
+      {{
+        $t('pages.applyFacultyStudentParking.uploadDocuments.plate_registered')
+      }}
     </p>
     <div class="registered-list border rounded vehicle-registration">
       <ul>
@@ -79,8 +98,8 @@ applicationData.value.selectedCommuteDistance = '5-10km'
   </section>
   <form class="mt-5">
     <div class="mb-3">
-      <label for="car_type" class="form-label">
-        {{ $t('pages.applyStaffParking.uploadDocuments.distance') }}
+      <label for="distance" class="form-label">
+        {{ $t('pages.applyFacultyStudentParking.uploadDocuments.distance') }}
       </label>
       <select
         class="form-select"
@@ -91,6 +110,70 @@ applicationData.value.selectedCommuteDistance = '5-10km'
         </option>
       </select>
     </div>
+    <div class="mb-3">
+      <p>
+        {{ $t('pages.applyFacultyStudentParking.uploadDocuments.upload') }}
+      </p>
+      <div
+        v-for="(file, index) in applicationData.document_list"
+        :key="index"
+        class="bg-secondary mb-3 d-flex justify-content-around justify-content-md-center align-items-center upload position-relative pointer"
+        @click="!file && $refs[`fileInput${index}`][0].click()"
+      >
+        <p class="fs-4 fw-bold px-1 py-3 align-content-center m-0">
+          {{
+            file?.name ||
+            $t('pages.applyFacultyStudentParking.uploadDocuments.upload') +
+              (index + 1)
+          }}
+        </p>
+
+        <div v-if="!file" class="img-container">
+          <img src="/images/upload/upload.svg" alt="upload" />
+        </div>
+
+        <button
+          v-if="file"
+          type="button"
+          class="btn btn-sm position-absolute top-25 end-0"
+          @click.stop="removeFile(index)"
+        >
+          X
+        </button>
+
+        <input
+          type="file"
+          :ref="`fileInput${index}`"
+          class="d-none"
+          @change="handleFileUpload($event, index)"
+        />
+      </div>
+    </div>
   </form>
   <router-link to="/query-links">查詢</router-link>
 </template>
+
+<style scoped>
+.vehicle-registration {
+  height: 100px;
+  overflow-y: auto;
+  scrollbar-width: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+}
+
+.img-container {
+  padding: 10px;
+  width: 60px;
+  height: 60px;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.pointer {
+  cursor: pointer;
+}
+</style>
