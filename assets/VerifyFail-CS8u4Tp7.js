9bbc61f059c680ea855fbab78779048c6b25f560
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-18 16:09:38 +0800

    Updates

---
+++ assets/VerifyFail-CS8u4Tp7.js
@@ -0,0 +1,1 @@
+import{_ as t,c as s,a,t as c,o as n}from"./index-c3cCMPYn.js";const o={},r={style:{height:"70vh"},class:"container d-flex align-items-center justify-content-center"},i={class:"fs-2"};function l(e,_){return n(),s("div",r,[a("h3",i,c(e.$t("pages.applyFacultyStudentParking.verify_fail")),1)])}const d=t(o,[["render",l]]);export{d as default};
