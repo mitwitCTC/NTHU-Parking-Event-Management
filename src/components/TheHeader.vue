475cca66f521bf79c7143d780f908abc0a2f408a
Eileen-Qian <yiqianc.14@gmail.com>
2024-11-29 10:29:04 +0800

    feat: Modify  style class for title and subtitle in header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -13,16 +13,6 @@ defineProps({
     default: false,
   },
 })
-import { useI18n } from 'vue-i18n'
-import { computed } from 'vue'
-const { locale } = useI18n()
-// 根據當前語系決定 title subtitle的字型大小
-const titleClass = computed(() => {
-  return locale.value === 'en' ? 'fs-3' : 'fs-2'
-})
-const subtitleClass = computed(() => {
-  return locale.value === 'en' ? 'fs-6' : 'fs-5'
-})
 </script>
 
 <template>
@@ -36,11 +26,11 @@ const subtitleClass = computed(() => {
         @click="$emit('goBack')"
         class="back-icon me-3"
       />
-      <div class="flex-grow-1 text-center">
+      <div class="flex-grow-1 text-start">
         <!-- 標題 -->
-        <p :class="titleClass">{{ title }}</p>
+        <p class="fs-3">{{ title }}</p>
         <!-- 副標題 -->
-        <h5 :class="subtitleClass">{{ subtitle }}</h5>
+        <h5 class="fs-6">{{ subtitle }}</h5>
       </div>
     </div>
     <!-- 默認插槽，顯示 HeaderButtonsSection -->
