da7a1ec62a81ff60910df46070308adf1fb14a87
Eileen-Qian <yiqianc.14@gmail.com>
2024-12-11 17:32:01 +0800

    feat: Update step2_1 login view for apply event

--- src/views/applyEvent/Step2_1Login.vue
+++ src/views/applyEvent/Step2_1Login.vue
@@ -1,5 +1,7 @@
 <script setup>
 import StepNavigator from '@/components/StepNavigator.vue'
+import { ref } from 'vue'
+import router from '@/router'
 const currentStep = 2
 
 // 定義步驟資料
@@ -21,8 +23,72 @@ const steps = [
     currentStepClass: 2,
   },
 ]
+const login_data = ref({
+  email: '',
+  phone_number: '',
+})
+
+const login_result = ref('')
+async function login() {
+  if (
+    login_data.value.email == 'test@gmail.com' &&
+    login_data.value.phone_number == '0909123456'
+  ) {
+    login_result.value = 'success'
+    router.push('/apply-event/step2_2')
+  } else {
+    login_result.value = 'fail'
+  }
+  clearLogin_result()
+}
+
+function clearLogin_result() {
+  setTimeout(() => {
+    login_result.value = ''
+  }, 2000)
+}
 </script>
 <template>
   <StepNavigator :currentStep="currentStep" :steps="steps" />
-  <div>登入</div>
+  <div
+    v-if="login_result"
+    :class="[
+      'alert',
+      login_result == 'success' ? 'alert-success' : 'alert-danger',
+    ]"
+    role="alert"
+  >
+    {{
+      login_result == 'success'
+        ? $t('pages.applyStaffParking.loginAndUpload.success')
+        : $t('pages.applyStaffParking.loginAndUpload.fail')
+    }}
+  </div>
+  <form>
+    <div class="mb-3">
+      <label for="email" class="form-label">
+        {{ $t('pages.applyEvent.login.email') }}
+      </label>
+      <input
+        type="text"
+        class="form-control"
+        id="email"
+        v-model="login_data.email"
+      />
+    </div>
+    <div class="mb-3">
+      <label for="phone_number" class="form-label">
+        {{ $t('pages.applyEvent.login.phone_number') }}
+      </label>
+      <input
+        type="text"
+        class="form-control"
+        id="phone_number"
+        v-model="login_data.phone_number"
+      />
+    </div>
+    <button class="btn btn-secondary w-100" @click.prevent="login">
+      {{ $t('pages.applyEvent.login.login') }}
+    </button>
+  </form>
 </template>
