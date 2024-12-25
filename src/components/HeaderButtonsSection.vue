2fe2a9a6ea810093e5b71b8de4ca95e3e8bbd60c
Eileen-Qian <yiqianc.14@gmail.com>
2024-12-25 12:34:35 +0800

    feat: Add button to return to application homepage on query homepage

--- src/components/HeaderButtonsSection.vue
+++ src/components/HeaderButtonsSection.vue
@@ -15,6 +15,15 @@ const formattedQueryLinks = computed(() => {
   }
   return text
 })
+const formattedHomeLinks = computed(() => {
+  const text = t('pages.query.goHomePage')
+
+  // 只在英文中處理換行
+  if (locale.value === 'en') {
+    return text.split('\n').join('<br />')
+  }
+  return text
+})
 
 const languageDisplay = computed(() => {
   return locale.value === 'zh' ? '中文/EN' : '中文/EN'
@@ -27,9 +36,10 @@ const route = useRoute()
 
 // 根據當前路由名稱來判斷是否顯示 query-btn
 const isHomeRoute = computed(() => route.name === 'Home')
+const isQueryHomeRoute = computed(() => route.name === 'QueryLinks')
 // 判斷按鈕是否顯示，來動態調整樣式
 const buttonsClass = computed(() => {
-  return isHomeRoute.value
+  return isHomeRoute.value || isQueryHomeRoute.value
     ? 'd-flex justify-content-between'
     : 'd-flex justify-content-end'
 })
@@ -37,12 +47,19 @@ const buttonsClass = computed(() => {
 
 <template>
   <div class="bg-light mt-0 py-2 header-btn-section">
-    <div class="container px-5 px-md-0">
+    <div class="container px-4 px-md-3">
       <div :class="buttonsClass">
         <div v-if="isHomeRoute" class="query-btn">
           <button class="btn btn-primary">
             <router-link to="query-links" class="text-white">
               <span v-html="formattedQueryLinks"></span>
+            </router-link>
+          </button>
+        </div>
+        <div v-if="isQueryHomeRoute" class="query-btn">
+          <button class="btn btn-primary">
+            <router-link to="/" class="text-white">
+              <span v-html="formattedHomeLinks"></span>
             </router-link>
           </button>
         </div>
