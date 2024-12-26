9927ec1e1a83e3ac6ad566e7c33455e9b1b01787
Eileen-Qian <yiqianc.14@gmail.com>
2024-12-26 11:45:46 +0800

    feat: Update paca API base url

--- src/pacaApi.js
+++ src/pacaApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiUrl =
-  import.meta.env.VITE_API_BASE_URL || 'http://hq.paca.com.tw:21080/api'
+  import.meta.env.VITE_API_BASE_URL || 'http://hq.paca.com.tw:18080/api'
 
 const pacaApi = axios.create({
   baseURL: apiUrl,
