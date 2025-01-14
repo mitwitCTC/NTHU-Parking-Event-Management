db22d815bc65b2af4f462407ab25546ba3ff230b
Eileen-Qian <yiqianc.14@gmail.com>
2025-01-14 23:35:07 +0800

    Configure API proxies for pacaApi and Api in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -13,4 +13,18 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/pacaApi': {
+        target: 'http://hq.paca.com.tw:21080/api', // paca 的根路由
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/pacaApi/, ''), // 去掉前綴 '/pacaApi'
+      },
+      '/api': {
+        target: 'http://122.116.23.30:4320', // 另一個 API 的根路由
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''), // 去掉前綴 '/api'
+      },
+    },
+  },
 })
